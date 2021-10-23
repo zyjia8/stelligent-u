02d683a161a0a33ac260634fe19b24c73ab3553a
Irene Jia <irene.jia@stelligent.com>
2021-10-22 19:13:19 -0700

    lab5.4.1

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-aws cloudformation update-stack --capabilities CAPABILITY_NAMED_IAM --stack-name irene-practice-stack --template-body file://25-ec2/ec2.yaml
+aws cloudformation create-stack --capabilities CAPABILITY_NAMED_IAM --stack-name irene-practice-stack --template-body file://25-ec2/ec2.yaml
 aws cloudformation wait stack-update-complete --stack-name irene-practice-stack
